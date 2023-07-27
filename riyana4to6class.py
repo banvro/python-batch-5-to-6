f1a71a78d0256357c5a3f8c40804a44716439554
Naresh Kumar <72598101+banvro@users.noreply.github.com>
2023-07-27 17:07:06 +0530

    Update riyana4to6class.py

--- riyana4to6class.py
+++ riyana4to6class.py
@@ -80,3 +80,59 @@
 """
 
 # print("hlooooooo")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# calculator: basic using if else
+
+print("******************Calculater**************")
+
+num1 = int(input("Enter first number : "))
+num2 = int(input("Enter second number : "))
+
+print("""
+      ......PLease choose any of one........
+    1) Adation
+    2) Substraction
+    3) Multiplication 
+    4) Devsion
+""")
+      
+choce = input("Enter what you want to do : ")
+# 3
+# "3" == "1"
+if choce == "1":
+    sum = num1 + num2
+    print("the sum is : ", sum)
+
+elif choce == "2":
+    sub = num1 - num2
+    print("The substraction is : ", sub)
+
+
+elif choce == "3":
+    mul = num1 * num2
+    print("The multiplication is : ", mul)
+
+
+elif choce == "4":
+    div = num1 / num2
+    print("The devsion is : ", div)
+
+else:
+    print("Something went wrong..")
