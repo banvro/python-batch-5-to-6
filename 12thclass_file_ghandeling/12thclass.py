53077627bbe6000be4d7ee470b9c5f6546c42731
Naresh Kumar <bnavro07@gmail.com>
2023-08-01 18:13:04 +0530

    Class 12 file handling

---
+++ 12thclass_file_ghandeling/12thclass.py
@@ -0,0 +1,70 @@
+# file handeling
+
+# a = 20
+# b = 30 
+
+# c = a + b
+
+# print(c)
+
+
+# fil = open("12thclass_file_ghandeling/myfile.txt", 'r')
+
+# print(fil.read(20))
+
+# print(fil.readline())
+# print(fil.readline())
+
+# for i in fil:
+#     print(i)
+
+
+
+
+# how to write in file 
+
+# w, a 
+
+# fil = open("12thclass_file_ghandeling/myfiles.txt", 'w')
+
+# fil.write("i am chnage ")
+
+
+# fil = open("12thclass_file_ghandeling/myfile1.txt", 'a')
+
+# fil.write("i am chnage again gain and ajbsj")
+
+
+
+# import os
+
+# os.remove("12thclass_file_ghandeling/myfile1.txt")
+# r, w, a 
+# open("", "")
+
+
+
+
+
+
+
+# Exception Handeling: 
+
+# 1000
+
+# 500 => keweri
+
+
+# try Exception
+
+# try:
+#     a = 20
+#     b = 30
+
+#     summ = a + b
+
+#     print("the sum is : ", summ)
+# except Exception as e:
+#     print(e)
+
+# print("i am moving on...")
