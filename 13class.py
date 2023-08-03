285791af3ffa0c4fddc9ffc1fee292d490e9f780
Naresh Kumar <bnavro07@gmail.com>
2023-08-03 17:43:12 +0530

    Python all prious sullyus cover

---
+++ 13class.py
@@ -0,0 +1,58 @@
+# Exception handeling :
+    # 1) compile time error 
+    # 2) LOgical error 
+    # 3) Run time error
+
+
+# compile time error :  syntex error
+
+# if a = 20
+    # print(a)
+
+# LOgical error  
+
+# a = 20
+# b = 0
+
+# c = a/b
+
+
+# Run time error: 
+
+# a = int(input("Enter you age : "))
+
+
+
+# a = 10
+
+# b = 1
+
+# # c = a / b
+# # print(c)
+
+# try:
+#     a = int(input("enter any number : "))
+#     c = a / b
+#     print(c)
+# except ZeroDivisionError as e:
+#     # print(e)
+#     print("you never devide anything by zero")
+# except ValueError as e:
+#     # print(e)
+#     print("you do not have to enter charcter")
+
+# except Exception as e:
+#     print(e)
+
+# finally:
+#     print("i am finnely")
+
+
+# print("this is me....")
+
+
+
+
+
+
+
---
+++ 14thclass.py
@@ -0,0 +1,106 @@
+# python and its history
+
+
+
+# Guido van Roussum
+
+# ABC Language
+# mudulo -c 
+# c
+# unix
+
+
+
+# 1980 ----- 1991
+
+
+# 1991 ---- python 0.9.0
+
+
+# 2000 === python 2
+
+# 2020
+
+
+# 2008 ---- python 3
+
+
+
+
+# print "hellow word"
+
+
+# print("hello word")
+
+# range()
+
+# xrange()
+
+# abc = 20
+
+# keywords
+
+# int float tuple list
+
+
+# {
+#     sksajfka
+#     asdksdja
+#     ajsdaksd
+# }
+
+
+
+
+# if 10 == 10:
+#     prlnfls
+#     sd fskdjf
+#     sdf skfd
+
+
+"""
+asdkajsbdkasd
+"""
+
+
+
+# def car():
+#     """
+#     bjhabjsdbjahsd
+#     asjdbakdbaskd
+#     asdjaskdjsadkjbas
+#     """
+#     print("xyz")
+
+
+# car()
+# help(input)
+
+
+# import math
+
+# print(math.sqrt(20))
+
+# ASSERT use for dubugging.....
+
+
+# assert 2 + 3 == 4, "this is not equal"
+
+
+# def isoo(a, b):
+#     c = a + b 
+    
+#     return c
+
+# xyz = isoo(2, 4)
+
+# print(xyz)
+
+# lambda
+
+# lambda argument : expression
+
+
+xy = lambda a, b : a + b
+
+print(xy(12, 21))
