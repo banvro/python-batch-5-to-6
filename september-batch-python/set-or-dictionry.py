9530f3eb6be7a5affcd0fd918755f03a207815d7
Naresh Kumar <banvro07@gmail.com>
2023-09-20 05:36:40 -0700

    set or dict

---
+++ september-batch-python/set-or-dictionry.py
@@ -0,0 +1,136 @@
+# List : ordered, allow duplicasy, mutable
+# Tuple : ordered, allow duplicasy, imutable
+
+# Set : unordered, do not allow duplicasy, mutable
+
+# collcation of multipal data in a single variable
+# {}
+
+
+# st = {10, 80, "hello", 100, 90, 80, 100, "car", 100.7, 10}
+
+# add()
+
+# st.add(2000)
+
+# st.pop()
+# st.remove(80)
+
+# print(st)
+
+# Dictionry :
+# ordered, do not allow duplicasy, mutable
+# {key : value}
+
+# dic = {"name" : "kriss","age" : 20, "number" : 9286423486923, "age" : 30
+# }
+
+# print(dic)
+
+# print(type(dic))
+
+# print(dic["age"])
+
+
+# JSON : 
+
+
+# dict = {
+#     1 : {
+#         "name" : "kriss",
+#         "age" : 20,
+#         "number" : 92836492364
+#     },
+#     2 : {
+#         "name" : "helo",
+#         "age" : 9,
+#         "number" : {
+#             "number1" : 92863482364, 
+#             "number2" : 28364238498
+#         }
+#     }
+# }
+# print(dict[2]["number"]["number1"])
+
+
+
+dic = {"name" : "kidd", "age" : 20, "number" : 29836498264}
+
+
+
+# print(dic.keys())
+
+# print(dic.values())
+
+# # dic["address"] = "this is a address"
+
+# # to delete data
+
+# dic.pop("age")
+
+# print(dic)
+
+
+
+
+# lst = [12, 23, 34, 100, 89, 67]
+
+
+# for i in range(10):
+#     print(i)
+#     print("helloooo")
+
+
+lst = ["name", "age", "number", "address"]
+
+tpl = ("kriss", 20, 9283489264, "this is address")
+
+dict = {}
+
+# output 
+
+{"name" : "kriss", "age" : 20, "number" : 9283489264, "address" : "this is address"}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
