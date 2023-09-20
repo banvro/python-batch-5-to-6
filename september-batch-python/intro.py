e582c52f3556867b3833ca707586393410fd8e59
Naresh Kumar <banvro07@gmail.com>
2023-09-20 04:55:00 -0700

    python introduxtion

---
+++ september-batch-python/intro.py
@@ -0,0 +1,102 @@
+# Python History :
+
+# 1980 ---  Guido Van Rossum 
+
+# ABC Language
+
+
+# C
+# Unix
+# objective.
+# ,
+
+# 1980 ----  1991 
+
+# Python 0.9.0
+
+# 2000   ----  python 2
+
+# 2020
+
+# 2008 ----- python 3
+
+
+
+
+# Unique features of python
+    # 1) simple
+    # 2) Gernal Perpose
+    # 3) High Level
+    # 4) object Orinted
+    # 5) Interpreated
+    # 6) Scriptng
+    # 7) Dynimically Typed
+    # 8) Case Senstive language..
+    
+    
+# #include <stdio.h>
+# #include <conio.h>
+
+# int main(){
+#     int a;
+#     int b, c;
+#     a = 10;
+#     b = 20;
+#     c = a + b
+#     printf("%d", c)
+# }
+
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+
+# 2) Gernal Perpose : various perposes
+
+# 1) Webiste devlopment  ::  Django, Flask
+# 2) MObile app backed  : DRF
+# 3) Desktop Applications : PYQT5
+# 4) AI   :  
+
+
+
+# Languages:
+    
+#     1) High level Lnaguage
+    
+#     2) Low level languages  
+#     machine language/ binary langugae( 01 )
+
+
+# interprater
+
+
+
+# comiler   
+
+
+
+#  Dynimically Typed : do not need to tell the system, what kind of data it holds
+
+# int a;
+# a = 10
+
+# a = 10
+
+# b = "kriss"
+
+
+# 8) Case Senstive language..
+
+# age = 10
+
+# Age = 30
+
+
+
+
+# what are the diffrences between compiler or interpreater
+
+# low level language and high level lanaguage...
