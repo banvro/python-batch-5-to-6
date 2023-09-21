62886bfe433684d51951c9359522c7fabfcb5788
Naresh Kumar <banvro07@gmail.com>
2023-09-21 05:13:51 -0700

    file hndeling

---
+++ september-batch-python/file handeling/file_handeling.py.py
@@ -0,0 +1,81 @@
+# dic = {
+#     1 : "this is one",
+#     "hello" : "this is second"
+# }
+
+# print(dic["hello"])
+
+# dic["address"] = "this is new value"
+
+# print(dic["address"])
+
+# lst = ["name", "age", "number"]
+
+# tpl = ("kriss", 20, 9812398123)
+
+
+# dic = {}
+
+# for i in range(3):
+#     dic[lst[i]] = tpl[i]
+
+# print(dic)
+
+
+# File Handeling : 
+
+# a = 10
+# b = 20
+# c = a + b
+# print(c)
+
+# txt 
+
+# open()
+
+
+# fil = open("abc2.txt", 'r')
+# print(fil.read())
+
+# print(fil.readline())
+# print(fil.readline())
+
+
+
+
+# 2) white mode 
+
+# fil = open("abc2.txt", "w")
+
+# fil.write("this is new data comming from write mehod..")
+
+
+
+# 3) append mode 
+
+# fil = open("abc5.txt", "a")
+# fil.write("\n oashdoashdiahsodiashod")
+
+# fil.close()
+
+
+
+
+# a = 2000
+# b = 1000
+# c = a + b 
+
+# c = str(c)
+
+# fil = open("store.txt", "w")
+# fil.write(c)
+# fil.close()
+
+
+
+
+
+
+
+
+
